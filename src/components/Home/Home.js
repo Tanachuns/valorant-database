1ef3901ec6471011953bc75666b5b4f283bbcb20
tanarakchuns <tanarak.chuns@gmail.com>
2022-11-30 09:58:07 +0700

    fix Agent Responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import Menu from "./Menu";
 
 const Home = (props) => {
   return (
-    <div className="container">
+    <div className="container home">
       <div className="home-img box">
         <h1>Home</h1>
       </div>

7f5917f9bc45ae7a3d42f772a0e20ef0768e6dcd
tanarakchuns <tanarak.chuns@gmail.com>
2022-11-30 15:53:56 +0700

    fix tooltip

--- src/components/Weapons/WeaponBox.js
+++ src/components/Weapons/WeaponBox.js
@@ -4,7 +4,7 @@ const WeaponBox = (props) => {
   return (
     <div className="weapon-box tooltip">
       <img width={110} src={props.item.displayIcon} alt="" />
-      <div class="top">
+      <div className="top">
         <h3>{props.item.displayName}</h3>
         <p>{props.item.shopData.cost}$</p>
         <p>Type: {props.item.shopData.categoryText}</p>

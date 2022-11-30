45fa20c5a01e076310359cac6e6ab7cb9745d1f8
tanarakchuns <tanarak.chuns@gmail.com>
2022-11-30 11:15:51 +0700

    add icon

--- src/components/Weapons/Weapons.js
+++ src/components/Weapons/Weapons.js
@@ -1,21 +1,32 @@
+import axios from "axios";
 import React from "react";
 import Loading from "../Loading/Loading";
+import "./Weapons.css";
 
 const Weapons = () => {
   const [isLoading, setIsLoading] = React.useState(true);
+  const [weaponList, setWeaponList] = React.useState();
   React.useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2500);
+    axios
+      .get("https://valorant-api.com/v1/weapons")
+      .then((response) => {
+        setWeaponList(response.data.data);
+      })
+      .then(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2500);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
+
+  console.log(weaponList);
   if (isLoading) {
     return <Loading />;
   } else {
-    return (
-      <div className="container">
-        <div>gamemode 1</div>
-      </div>
-    );
+    return <div className="container weapon"></div>;
   }
 };
 export default Weapons;
